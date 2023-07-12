dba26efcec4dbff5bf75d31a3b1e31bebce6bae7
yessllar2023 <yesseniamillar@gmail.com>
2023-07-12 19:16:29 -0400

    Feat: agrego console.log a secciones de diagrama en archivo mdlinks.js y considero process.argv en node con archivo index.js

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ import {mdLinks} from "./mdLinks.js"; // requiere
 //module.exports = () => {
   // ...
 //};
-mdLinks ();
+mdLinks('README.md', true)
+.catch((err)=>console.log(err))
+.then((res)=>console.log(res))
 console.log("hola")
--- mdLinks.js
+++ mdLinks.js
@@ -1,3 +1,35 @@
 //function
-export const mdLinks = (path, options) => {//modules.exports
-}
+import fs from "fs";
+import path, { resolve } from 'path'
+export const mdLinks = (doc, options) => {
+  // console.log(doc, options);
+
+  return new Promise((resolve, reject) => {
+    //identificar si la ruta existe
+    if (fs.existsSync(doc)) {
+    //   console.log("El archivo EXISTE!", );
+    // es absoluta???
+      const docAbsolute = path.resolve(doc)
+      // es md???
+      if(path.extname(doc)=== '.md'){
+        //resolve(docAbsolute)
+     
+        // comentar resolve L16
+        // investigar como se lee el contenido de un achivo en node.js
+        // para leer el contendio usar docAbsolute
+      }else{
+        reject('Invalido')
+      }
+      
+      
+    } else {
+    //   console.log("El archivo NO EXISTE!");
+      reject('Invalido El archivo NO EXISTE!')
+
+    }
+  });
+};
+
+// modules.exports = () => {
+
+// }
