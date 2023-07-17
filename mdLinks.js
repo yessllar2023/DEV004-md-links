3c01d5f22526e1f785ae28bffafc5950e0cf17cf
yessllar2023 <yesseniamillar@gmail.com>
2023-07-17 18:50:03 -0400

    Feat: tenemos en proceso funcion mdlinks como promesa, creado comprobarRutas, crear encontrarLinks y comprobarLinks

--- mdLinks.js
+++ mdLinks.js
@@ -1,45 +1,56 @@
+//archivo mdLinks.js
 import fs from "fs";
 import path, { resolve } from "path";
 
 export const comprobarRutas = (doc) => {
   return new Promise((resolve, reject) => {
-    //identificar si la ruta existe
-    if (fs.existsSync(doc)) {
-      // console.log("El archivo EXISTE!");
-      // es absoluta???
-      const docAbsoluto = path.resolve(doc);
-      // console.log(docAbsolute)
-      // es md???
-      if (path.extname(docAbsoluto) === ".md") {
-        resolve(docAbsoluto);
-        // para leer el contenido usar docAbsolute
-      } else {
-        console.log("no se encontraron archivos md");
-      }
+  //identificar si la ruta existe
+  if (fs.existsSync(doc)) {
+    // console.log("El archivo EXISTE!", );
+    // es absoluta???
+    const docAbsoluto = path.resolve(doc);
+    // console.log(docAbsolute)
+    // es md???
+    if (path.extname(docAbsoluto) === ".md") {
+      resolve(docAbsoluto);
+      // para leer el contenido usar docAbsolute
     } else {
-      console.log("El archivo NO EXISTE!");
-      // reject('Invalido El archivo NO EXISTE!')
+      console.log("no se encontraron archivos md");
     }
+  } else {
+    //console.log("El archivo NO EXISTE!");
+   reject('Invalido El archivo NO EXISTE!')
+  }
   });
 };
 
-export const encontrarLinks = (doc) => {};
+//aqui array con objetos
+//los links son extraidos a un objeto y se almacena el objeto en un arreglo.
+//¿como extraerlos?
+//export const encontrarLinks = (archivo) => {
+//};
 
-export const comprobarLinks = (doc) => {
+//validacion
+//¿contiene links?
+//los links (todos) estan dentro de un arreglo
+//validamos los links
+//export const comprobarLinks = (links) => {
   //aqui se llamaran
-};
+//};
 
 export const mdLinks = (doc) => {
   return new Promise((resolve, reject) => {
     comprobarRutas(doc)
-      .then((archivo) => {
-        console.log(archivo);
-      })
-      .catch((error) =>
-        reject(console.log("no se pudo llevar a cabo este proceso"))
-      );
+    .then((archivo) => {
+      //activarlos solo al ser creados
+     // return encontrarLinks(archivo)
+    })
+    //.then(links =>{
+      //dar retorno con resolve solo al ser creada comprobarlinks
+    // console.log(links)
+    //})
+    .catch(error=>reject(console.log("no se pudo llevar a cabo este proceso")))
   });
 };
 // modules.exports = () => {
-
-// }
+//}
